cb16e74be636f3d57da683f35a82c1545b21d375
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-09-07 21:10:27 +0300

    edit login page

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render(request , 'website/home.html')
+    # Checking if the user is loged in or not
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        #authentication
+        user = authenticate(request , username=username , password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "u succesfully loged in :)")
+            return redirect('home')
+        else:
+            messages.info(request, "there was an error and we cant log u in :(" )
+            return redirect('home')
+    else:
+        return render(request , 'website/home.html')
+
+def login_user(request): 
+    pass
+
+def logout_user(request):
+    pass
